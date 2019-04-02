d71eeb24dd2b2c00d0a86a3eaaec82b15fbbe2ee
ekko <270114440@qq.com>
2019-04-02 15:45:48 +0800

    增加pub view层

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'view'))
+app.set('view engine', 'pug')
 
 let myLogger = function (req, res, next) {
   console.log('=====req====')
@@ -10,15 +14,21 @@ let timer = function (req, res, next) {
   req.requestTime = Date.now()
   next()
 }
+let indexPug = function (req, res, next) {
+  res.render('index', { title: '23333' })
+}
 
 app.use(myLogger)
 app.use(timer)
+app.use(indexPug)
 
 app.get('/', function (req, res) {
   let json = {
     text: 'Hello World!',
-    time: req.requestTime
+    time: req.requestTime,
+    id: req.query.id
   }
+  // res.render('index', { title: '23333' })
   res.send(json)
 })
 
